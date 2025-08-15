a4bd64891f1fc62d914ca6fe16a1787455106a31
unbekannt3 <me@unbekannt3.eu>
2025-08-16 01:49:33 +0200

    chore: webpack proper minification of css/html in literals

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,98 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-	entry: './src/index.js',
-	mode: 'development',
-	output: {
-		filename: 'room-card-minimalist.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	devServer: {
-		static: path.join(__dirname, 'dist'),
-		port: 8080,
-		host: '0.0.0.0',
-		headers: {
-			'Access-Control-Allow-Origin': '*',
+module.exports = (env, argv) => {
+	const isProduction = argv.mode === 'production';
+
+	return {
+		entry: './src/index.js',
+		mode: argv.mode || 'development',
+		output: {
+			filename: 'room-card-minimalist.js',
+			path: path.resolve(__dirname, 'dist'),
+			clean: true,
 		},
-		// Error overlays
-		client: {
-			overlay: false,
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					use: [
+						{
+							loader: 'babel-loader',
+							options: {
+								presets: [
+									[
+										'@babel/preset-env',
+										{
+											targets: {
+												browsers: ['> 1%', 'last 2 versions'],
+											},
+										},
+									],
+								],
+								plugins: [
+									[
+										'babel-plugin-template-html-minifier',
+										{
+											modules: {
+												'lit-element': ['html', 'css'],
+												lit: ['html', 'css'],
+											},
+											htmlMinifier: {
+												caseSensitive: true,
+												collapseWhitespace: true,
+												conservativeCollapse: true,
+												minifyCSS: true,
+												minifyJS: false,
+												removeAttributeQuotes: false,
+												removeComments: true,
+												removeEmptyAttributes: true,
+												removeOptionalTags: false,
+												removeRedundantAttributes: true,
+												removeScriptTypeAttributes: false,
+												removeStyleLinkTypeAttributes: false,
+												sortAttributes: false,
+												sortClassName: false,
+											},
+											logOnFailure: true,
+										},
+									],
+								],
+							},
+						},
+					],
+					exclude: /node_modules/,
+				},
+			],
 		},
-	},
+		optimization: {
+			minimize: isProduction,
+			minimizer: [
+				new TerserPlugin({
+					terserOptions: {
+						compress: {
+							drop_console: isProduction,
+							drop_debugger: isProduction,
+						},
+						format: {
+							comments: false,
+						},
+					},
+					extractComments: false,
+				}),
+			],
+		},
+		devServer: {
+			static: path.join(__dirname, 'dist'),
+			port: 8080,
+			host: '0.0.0.0',
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+			// Error overlays
+			client: {
+				overlay: false,
+			},
+		},
+	};
 };
